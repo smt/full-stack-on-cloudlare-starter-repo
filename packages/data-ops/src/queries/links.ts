7fad069e579887570121545f52855880cdc58165
Stephen Tudor <smt@smt.io>
2025-09-24 17:00:18 -0400

    feat(data-ops): add DB query to get links

--- packages/data-ops/src/queries/links.ts
+++ packages/data-ops/src/queries/links.ts
@@ -1,6 +1,7 @@
 import { getDb } from "@/db/database";
 import { links } from "@/drizzle-out/schema";
 import { CreateLinkSchemaType } from "@/zod/links";
+import { and, desc, eq, gt } from "drizzle-orm";
 import { nanoid } from "nanoid";
 
 export async function createLink(
@@ -15,4 +16,35 @@ export async function createLink(
     destinations: JSON.stringify(data.destinations),
   });
   return id;
+}
+
+export async function getLinks(accountId: string, createdBefore?: string) {
+  const db = getDb();
+
+  const conditions = [eq(links.accountId, accountId)];
+
+  if (createdBefore) {
+    conditions.push(gt(links.created, createdBefore));
+  }
+
+  const result = await db
+    .select({
+      linkId: links.linkId,
+      destinations: links.destinations,
+      created: links.created,
+      name: links.name,
+    })
+    .from(links)
+    .where(and(...conditions))
+    .orderBy(desc(links.created))
+    .limit(25);
+
+  return result.map((link) => ({
+    ...link,
+    lastSixHours: Array.from({ length: 6 }, () =>
+      Math.floor(Math.random() * 100),
+    ),
+    linkClicks: 6,
+    destinations: Object.keys(JSON.parse(link.destinations as string)).length,
+  }));
 }
