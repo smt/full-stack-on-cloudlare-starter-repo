3608bcd64319409c79e2db23f171aa7fe0eab444
Stephen Tudor <smt@smt.io>
2025-09-24 22:11:48 -0400

    feat(data-service): initialize the database in the DataService constructor

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,7 +1,13 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { App } from './hono/app';
+import { initDatabase } from '@repo/data-ops/database';
 
 export default class DataService extends WorkerEntrypoint<Env> {
+  constructor(ctx: ExecutionContext, env: Env) {
+    super(ctx, env);
+    initDatabase(env.DB);
+  }
+
   fetch(request: Request) {
     return App.fetch(request, this.env, this.ctx);
   }
