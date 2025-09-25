d25d8d5c4e67c685247f7dfe97740c6b6bb525ef
Stephen Tudor <smt@smt.io>
2025-09-24 22:12:43 -0400

    feat(data-service): query link info from DB

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,18 +1,10 @@
+import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
-const UNKNOWN = 'unknown';
-
 App.get('/:id', async (c) => {
-  console.log(JSON.stringify(c.req.raw.cf));
-  const cf = c.req.raw.cf;
-  const country = cf?.country ?? UNKNOWN;
-  const lat = cf?.latitude ?? UNKNOWN;
-  const lng = cf?.longitude ?? UNKNOWN;
-  return c.json({
-    country,
-    lat,
-    lng,
-  });
+  const id = c.req.param('id');
+  const linkInfoFromDb = await getLink(id);
+  return c.json(linkInfoFromDb);
 });
