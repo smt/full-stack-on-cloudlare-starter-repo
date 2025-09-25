0b54b25d7700c6f5c414d3424852b54d0dd4a20b
Stephen Tudor <smt@smt.io>
2025-09-24 21:51:49 -0400

    feat(data-service): access Cloudflare request data

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -2,8 +2,17 @@ import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
+const UNKNOWN = 'unknown';
+
 App.get('/:id', async (c) => {
+  console.log(JSON.stringify(c.req.raw.cf));
+  const cf = c.req.raw.cf;
+  const country = cf?.country ?? UNKNOWN;
+  const lat = cf?.latitude ?? UNKNOWN;
+  const lng = cf?.longitude ?? UNKNOWN;
   return c.json({
-    message: 'Hello from Data Service!',
+    country,
+    lat,
+    lng,
   });
 });
