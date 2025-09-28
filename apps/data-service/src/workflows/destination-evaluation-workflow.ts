07cafb2f3a0cb637107e15d8714e421059721ccb
Stephen Tudor <smt@smt.io>
2025-09-28 15:36:16 -0400

    feat(data-service): update workflow to use the browser rendering helper with the payload data

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,12 +1,10 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
+import { collectDestinationInfo } from '@/helpers/browser-render';
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-  async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep): Promise<void> {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+  async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep): Promise<void> {
     const collectedData = await step.do('Collect rendered destination page data', async () => {
-      console.log('Collecting rendered destination page data...');
-      return {
-        dummydata: 'dummydata',
-      };
+      return collectDestinationInfo(this.env, event.payload.destinationUrl);
     });
 
     console.log('Collected Data:', collectedData);
