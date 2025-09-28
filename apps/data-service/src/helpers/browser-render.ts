34e54bcc3fc07b0329915ce64145b7ce270e0d03
Stephen Tudor <smt@smt.io>
2025-09-28 15:35:31 -0400

    feat(data-service): add helper for rendering the page with puppeteer

---
+++ apps/data-service/src/helpers/browser-render.ts
@@ -0,0 +1,21 @@
+import puppeteer from '@cloudflare/puppeteer';
+
+export async function collectDestinationInfo(env: Env, destinationUrl: string) {
+  const browser = await puppeteer.launch(env.VIRTUAL_BROWSER);
+  const page = await browser.newPage();
+
+  // waits until there are no more than 2 network connections for at least 500 ms.
+  const response = await page.goto(destinationUrl, { waitUntil: 'networkidle2' });
+
+  const bodyText = (await page.$eval('body', (el) => el.innerText)) as string;
+  const html = await page.content();
+  const status = response ? response.status() : 0;
+
+  await browser.close();
+
+  return {
+    bodyText,
+    html,
+    status,
+  };
+}
