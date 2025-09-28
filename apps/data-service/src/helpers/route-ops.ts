ae5621b8a4dd3eee608eaa60133761f66ab8a35f
Stephen Tudor <smt@smt.io>
2025-09-28 10:49:09 -0400

    feat(data-service): add helpers to save/retrieve linkInfo to/from cache

--- apps/data-service/src/helpers/route-ops.ts
+++ apps/data-service/src/helpers/route-ops.ts
@@ -1,4 +1,42 @@
-import { LinkSchemaType } from '@repo/data-ops/zod-schema/links';
+import { LinkSchemaType, linkSchema } from '@repo/data-ops/zod-schema/links';
+import { getLink } from '@repo/data-ops/queries/links';
+
+async function getLinkInfoFromKv(env: Env, id: string) {
+  const linkInfo = await env.CACHE.get(id);
+  if (!linkInfo) {
+    console.log('Cache miss for link ID:', id);
+    return null;
+  }
+  console.log('Cache hit for link ID:', id);
+  const parsedLinkInfo = linkSchema.safeParse(JSON.parse(linkInfo));
+  return parsedLinkInfo.success ? parsedLinkInfo.data : null;
+}
+
+const TTL_TIME = 60 * 60 * 24; // 1 day in seconds
+
+async function saveLinkInfoToKv(env: Env, id: string, linkInfo: LinkSchemaType) {
+  try {
+    await env.CACHE.put(id, JSON.stringify(linkInfo), {
+      expirationTtl: TTL_TIME,
+    });
+  } catch (error) {
+    console.error('Error saving link info to KV:', error);
+  }
+}
+
+export async function getRoutingDestinations(env: Env, id: string) {
+  const linkInfo = await getLinkInfoFromKv(env, id);
+  if (linkInfo) {
+    return linkInfo;
+  }
+
+  const linkInfoFromDb = await getLink(id);
+  if (!linkInfoFromDb) {
+    return null;
+  }
+  await saveLinkInfoToKv(env, id, linkInfoFromDb);
+  return linkInfoFromDb;
+}
 
 export function getDestinationForCountry(linkInfo: LinkSchemaType, countryCode?: string) {
   if (!countryCode) {
