96b36d28e007c0eb00ee494b005a8cadaf13f441
Stephen Tudor <smt@smt.io>
2025-09-28 17:47:39 -0400

    feat(data-service): add workflow step to save destination evaluation data in D1

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,9 +1,13 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
   async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep): Promise<void> {
+    initDatabase(this.env.DB);
+
     const collectedData = await step.do('Collect rendered destination page data', async () => {
       return collectDestinationInfo(this.env, event.payload.destinationUrl);
     });
@@ -17,5 +21,15 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
         return aiDestinationChecker(this.env, collectedData.bodyText);
       },
     );
+
+    const evaluationId = await step.do('Save evaluation in database', async () => {
+      return await addEvaluation({
+        linkId: event.payload.linkId,
+        status: aiStatus.status,
+        reason: aiStatus.statusReason,
+        accountId: event.payload.accountId,
+        destinationUrl: event.payload.destinationUrl,
+      });
+    });
   }
 }
