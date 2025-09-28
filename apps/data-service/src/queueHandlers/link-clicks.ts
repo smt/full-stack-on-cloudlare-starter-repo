ed0d6c99e422f16054f6b3d2a9785406fb45b0d8
Stephen Tudor <smt@smt.io>
2025-09-28 13:52:50 -0400

    feat(data-service): handle valid LINK_CLICK events

--- apps/data-service/src/queueHandlers/link-clicks.ts
+++
@@ -1,6 +0,0 @@
-import { addLinkClick } from '@repo/data-ops/queries/links';
-import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
-
-export async function handleLinkClick(env: Env, event: LinkClickMessageType) {
-  await addLinkClick(event.data);
-}
