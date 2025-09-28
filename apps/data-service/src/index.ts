b54e542e7c9b0e4b7944f765f9d09962532ae7cf
Stephen Tudor <smt@smt.io>
2025-09-28 13:52:50 -0400

    feat(data-service): handle valid LINK_CLICK events

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,6 +1,8 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { App } from './hono/app';
 import { initDatabase } from '@repo/data-ops/database';
+import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
+import { handleLinkClick } from './queue-handlers/link-clicks';
 
 export default class DataService extends WorkerEntrypoint<Env> {
   constructor(ctx: ExecutionContext, env: Env) {
@@ -14,7 +16,25 @@ export default class DataService extends WorkerEntrypoint<Env> {
 
   async queue(batch: MessageBatch<unknown>): Promise<void> {
     for (const message of batch.messages) {
-      console.log(`Queue Message ${message.id}:`, message.body);
+      const parsedEvent = QueueMessageSchema.safeParse(message.body);
+      if (parsedEvent.success) {
+        const event = parsedEvent.data;
+        console.log(`Queue Message ${message.id}:`, event);
+
+        switch (event.type) {
+          case 'LINK_CLICK': {
+            console.log(`Begin handling event type: ${event.type}`);
+            await handleLinkClick(this.env, event);
+            console.log(`Done handling event type: ${event.type}`);
+            break;
+          }
+          default: {
+            console.warn(`Unhandled event type: ${event.type}`);
+          }
+        }
+      } else {
+        console.error(parsedEvent.error);
+      }
     }
   }
 }
