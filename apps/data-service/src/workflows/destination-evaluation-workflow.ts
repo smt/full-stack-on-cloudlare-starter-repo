787dbeff374793ce8c1234766f0de869e670c68d
Stephen Tudor <smt@smt.io>
2025-09-28 17:19:38 -0400

    feat(data-service): update workflow with a new step to use the AI destination checker

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,5 +1,6 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
   async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep): Promise<void> {
@@ -7,6 +8,14 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
       return collectDestinationInfo(this.env, event.payload.destinationUrl);
     });
 
-    console.log('Collected Data:', collectedData);
+    const aiStatus = await step.do(
+      'Use AI to check status of page',
+      {
+        retries: { limit: 0, delay: 0 },
+      },
+      async () => {
+        return aiDestinationChecker(this.env, collectedData.bodyText);
+      },
+    );
   }
 }
