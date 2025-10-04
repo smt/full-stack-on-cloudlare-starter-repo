cf8c781acc6ccc41d8613b526630edc1ee5d2cce
Stephen Tudor <smt@smt.io>
2025-10-04 15:01:11 -0400

    feat(user-application): add click-socket request handler that proxies to data-service via the service binding

--- apps/user-application/worker/hono/app.ts
+++ apps/user-application/worker/hono/app.ts
@@ -14,3 +14,10 @@ App.all("/trpc/*", (c) => {
       createContext({ req: c.req.raw, env: c.env, workerCtx: c.executionCtx }),
   });
 });
+
+App.get("/click-socket", async (c) => {
+  const headers = new Headers(c.req.raw.headers);
+  headers.set("account-id", "1234567890"); // hardcoded for testing
+  const proxiedRequest = new Request(c.req.raw, { headers });
+  return c.env.BACKEND_SERVICE.fetch(proxiedRequest);
+});
