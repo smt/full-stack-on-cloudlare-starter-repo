bed6fa0659ed83430cef10ceb5f9e5944df5e635
Stephen Tudor <smt@smt.io>
2025-10-04 10:54:26 -0400

    feat(data-service): add new LinkClickTracker durable object class

---
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -0,0 +1,56 @@
+import { DurableObject } from 'cloudflare:workers';
+
+export class LinkClickTracker extends DurableObject<Env> {
+  sql: SqlStorage;
+
+  constructor(ctx: DurableObjectState, env: any) {
+    super(ctx, env);
+    this.sql = ctx.storage.sql;
+
+    ctx.blockConcurrencyWhile(async () => {
+      this.sql.exec(`
+        CREATE TABLE IF NOT EXISTS geo_link_clicks (
+          latitude REAL NOT NULL,
+          longitude REAL NOT NULL,
+          country TEXT NOT NULL,
+          time INTEGER NOT NULL
+        )
+      `);
+    });
+  }
+
+  async addLinkClick(latitude: number, longitude: number, country: string, time: number) {
+    this.sql.exec(
+      `
+      INSERT INTO geo_link_clicks (latitude, longitude, country, time)
+      VALUES (?, ?, ?, ?)
+      `,
+      latitude,
+      longitude,
+      country,
+      time,
+    );
+  }
+
+  async fetch(_: Request) {
+    const query = `
+      SELECT *
+      FROM geo_link_clicks
+      limit 100
+    `;
+
+    const cursor = this.sql.exec(query);
+    const results = cursor.toArray();
+
+    return new Response(
+      JSON.stringify({
+        clicks: results,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
+}
