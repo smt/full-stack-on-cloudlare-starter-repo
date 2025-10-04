b2706a65a404f89e171f40e07e81d60f1e9e95de
Stephen Tudor <smt@smt.io>
2025-10-04 14:28:49 -0400

    feat(data-service): add query to get the most recent clicks from sql storage

---
+++ apps/data-service/src/helpers/durable-queries.ts
@@ -0,0 +1,19 @@
+import { durableObjectGeoClickArraySchema } from '@repo/data-ops/zod-schema/links';
+
+export function getRecentClicks(sqlStorage: SqlStorage, offsetTime: number = 0, limit: number = 50) {
+  const query = `
+    SELECT latitude, longitude, country, time
+    FROM geo_link_clicks
+    WHERE time > ?
+    ORDER BY time DESC
+    LIMIT ?
+  `;
+
+  const cursor = sqlStorage.exec(query, offsetTime, limit);
+
+  const clicks = durableObjectGeoClickArraySchema.parse(cursor.toArray());
+  const mostRecentTime = clicks.length > 0 ? clicks[0].time : offsetTime;
+  const oldestTime = clicks.length > 0 ? clicks[clicks.length - 1].time : offsetTime;
+
+  return { clicks, mostRecentTime, oldestTime };
+}
